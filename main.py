4b8c1d00b3221f24a2606b521151a734322bd5a7
MattMorgan <matt@morgan.team>
2017-06-05 18:27:53 -0700

    Add name extraction

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-
+import slate
+import re
 
 # First, find the file.
 
 # Then, extract text from the file.
+with open('streamPDF.pdf', 'rb') as f:
+    raw = slate.PDF(f)
+    #print(raw[0])
 
 # Parse the text.
+
+students = []
+for page in raw:
+    name = re.search("Progress Report For (\D*) \(", page).group(1)
+    students.append({'name': name})
+print(students)
 
 # Analyze the text.
 
